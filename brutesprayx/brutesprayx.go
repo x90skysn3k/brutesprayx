64ae69244b5f494d6d1cfbdbd5541cf87876fbb6
t1d3nio <t1d3nio@gmail.com>
2023-12-31 20:58:33 -0600

    refactoring

--- brutesprayx/brutesprayx.go
+++ brutesprayx/brutesprayx.go
@@ -15,32 +15,9 @@ import (
 	"github.com/x90skysn3k/brutesprayx/banner"
 	"github.com/x90skysn3k/brutesprayx/brute"
 	"github.com/x90skysn3k/brutesprayx/modules"
-	"github.com/x90skysn3k/brutesprayx/parse"
 )
 
 var version = "v2.1.0"
-
-var NAME_MAP = map[string]string{
-	"ms-sql-s":       "mssql",
-	"microsoft-ds":   "smbnt",
-	"cifs":           "smbnt",
-	"postgresql":     "postgres",
-	"smtps":          "smtp",
-	"submission":     "smtp",
-	"imaps":          "imap",
-	"pop3s":          "pop3",
-	"iss-realsecure": "vmauthd",
-	"snmptrap":       "snmp",
-	"mysql":          "mysql",
-	//"ms-wbt-server":  "rdp",
-}
-
-func mapService(service string) string {
-	if mappedService, ok := NAME_MAP[service]; ok {
-		return mappedService
-	}
-	return service
-}
 
 func contains(s []string, e string) bool {
 	for _, a := range s {
@@ -51,94 +28,11 @@ func contains(s []string, e string) bool {
 	return false
 }
 
-func writeToFile(filename string, content string) error {
-	timestamp := time.Now().Format("2006010215")
-	dir := "output"
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		err := os.Mkdir(dir, 0755)
-		if err != nil {
-			return err
-		}
-	}
-	filename = filepath.Join(dir, filename+"_"+timestamp)
-	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = file.WriteString(content)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
 func isFile(fileName string) bool {
 	if _, err := os.Stat(fileName); err == nil && filepath.Ext(fileName) == "" {
 		return true
 	}
 	return false
-}
-
-func runbrute(h parse.Host, u string, p string) {
-	service := mapService(h.Service)
-	var result bool
-
-	switch service {
-	case "ssh":
-		result = brute.BruteSSH(h.Host, h.Port, u, p)
-	case "ftp":
-		result = brute.BruteFTP(h.Host, h.Port, u, p)
-	case "mssql":
-		result = brute.BruteMSSQL(h.Host, h.Port, u, p)
-	case "telnet":
-		result = brute.BruteTelnet(h.Host, h.Port, u, p)
-	case "smbnt":
-		result = brute.BruteSMB(h.Host, h.Port, u, p)
-	case "postgres":
-		result = brute.BrutePostgres(h.Host, h.Port, u, p)
-	case "smtp":
-		result = brute.BruteSMTP(h.Host, h.Port, u, p)
-	case "imap":
-		result = brute.BruteIMAP(h.Host, h.Port, u, p)
-	case "pop3":
-		result = brute.BrutePOP3(h.Host, h.Port, u, p)
-	case "snmp":
-		result = brute.BrutePOP3(h.Host, h.Port, u, p)
-	case "mysql":
-		result = brute.BruteMYSQL(h.Host, h.Port, u, p)
-	case "vmauthd":
-		result = brute.BruteVMAuthd(h.Host, h.Port, u, p)
-	//case "rdp":
-	//	result = brute.BruteRDP(h.Host, h.Port, u, p)
-	default:
-		//fmt.Printf("Unsupported service: %s\n", h.Service)
-		return
-	}
-
-	printResult(service, h.Host, h.Port, u, p, result)
-}
-
-func printResult(service string, host string, port int, user string, pass string, result bool) {
-
-	if result {
-		pterm.Success.Println("Attempt", service, "SUCCESS on host", host, "port", port, "with username", user, "and password", pass, getResultString(result))
-		content := fmt.Sprintf("Attempt %s SUCCESS on host %s port %d with username %s and password %s %s\n", service, host, port, user, pass, getResultString(result))
-		filename := filepath.Base(host)
-		writeToFile(filename, content)
-	}
-
-	pterm.Color(pterm.FgLightRed).Println("Attempt", service, "on host", host, "port", port, "with username", user, "and password", pass, getResultString(result))
-
-}
-
-func getResultString(result bool) string {
-	if result {
-		return "succeeded"
-	} else {
-		return "failed"
-	}
 }
 
 func Execute() {
@@ -171,7 +65,7 @@ func Execute() {
 		return nil
 	}
 
-	hosts, err := parse.ParseFile(*file)
+	hosts, err := modules.ParseFile(*file)
 	if err != nil && *file != "" {
 		fmt.Println("Error parsing file:", err)
 		os.Exit(1)
@@ -209,13 +103,13 @@ func Execute() {
 		passwords = modules.GetPasswordsFromDefaultWordlist(version)
 	}
 
-	var hostsList []parse.Host
+	var hostsList []modules.Host
 	for h := range hosts {
 		hostsList = append(hostsList, h)
 	}
 
 	if *host != "" {
-		var hostObj parse.Host
+		var hostObj modules.Host
 		if err := hostObj.Parse(*host); err != nil {
 			fmt.Println("Error parsing host:", err)
 			os.Exit(1)
@@ -241,7 +135,7 @@ func Execute() {
 	for _, h := range hostsList {
 		wg.Add(1)
 		sem <- struct{}{}
-		go func(h parse.Host) {
+		go func(h modules.Host) {
 			defer func() {
 				<-sem
 				wg.Done()
@@ -250,19 +144,19 @@ func Execute() {
 				for _, p := range passwords {
 					wg.Add(1)
 					sem <- struct{}{}
-					go func(h parse.Host, u string, p string) {
+					go func(h modules.Host, u string, p string) {
 						defer func() {
 							<-sem
 							wg.Done()
 							bar.Increment()
 						}()
-						service := mapService(h.Service)
+						service := brute.MapService(h.Service)
 						if *serviceType != "all" && !contains(getSupportedServices(*serviceType), service) {
 							return
 						}
 						bruteDone := make(chan bool)
 						go func() {
-							runbrute(h, u, p)
+							brute.RunBrute(h, u, p)
 							bruteDone <- true
 						}()
 
